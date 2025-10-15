4820ce0be559102d2d1fa63d19123864ca0c1cbe
vukha12 <kha15964@gmail.com>
2025-10-15 11:14:51 +0700

    done comments

--- wordpress_data/wp-content/themes/twentytwenty/singular.php
+++ wordpress_data/wp-content/themes/twentytwenty/singular.php
@@ -25,15 +25,54 @@ get_header();
 			get_template_part('template-parts/content', get_post_type());
 		}
 	}
-
 	?>
 	<?php
-	if (is_singular('post')) { // Chỉ hiển thị cho post
-		if (comments_open() || get_comments_number()) {
-			comments_template();
-		}
+	if (post_password_required()) {
+		return;
 	}
 	?>
+
+	<!-- Comment Form (Bootstrap Styled) -->
+	<div class="container d-flex justify-content-center align-items-center w-75 h-75">
+		<section class="card my-5 shadow-sm comments ">
+			<div class="card-header">
+				<ul class="nav nav-tabs card-header-tabs">
+					<li class="nav-item">
+						<a class="nav-link active" id="posts-tab" href="#posts">
+							<h2>Make a Post</h2>
+						</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="card-body">
+				<?php
+				comment_form(array(
+					'title_reply' => '',
+					'comment_field' => '
+                <div class="form-group">
+                    <textarea id="comment" name="comment" class="form-control comments-post" rows="6" placeholder="What are you thinking..." required></textarea>
+                </div>',
+					'fields' => array(
+						'author' => '
+                    <div class="form-group">
+                        <input id="author" name="author" type="text" class="form-control" placeholder="Your name" required>
+                    </div>',
+						'email' => '
+                    <div class="form-group">
+                        <input id="email" name="email" type="email" class="form-control" placeholder="Your email" required>
+                    </div>',
+					),
+					'submit_button' => '<div class="text-right"><button type="submit" class="btn btn-primary px-4"><h3>share</h3></button></div>',
+					'logged_in_as' => '',
+					'comment_notes_before' => '',
+					'comment_notes_after' => '',
+				));
+				?>
+			</div>
+		</section>
+	</div>
+
 
 </main><!-- #site-content -->
 
--- wordpress_data/wp-content/themes/twentytwenty/template-parts/content.php
+++ wordpress_data/wp-content/themes/twentytwenty/template-parts/content.php
@@ -18,6 +18,25 @@ if (!is_single()) {
 }
 ?>
 
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+
+	<title>Test2</title>
+
+	<style>
+		.h7 {
+			font-size: 0.9rem
+		}
+	</style>
+
+</head>
+
 <article <?php post_class($class); ?> id="post-<?php the_ID(); ?>">
 
 	<?php if (!is_single()) : ?>
