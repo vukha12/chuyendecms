4571a7e3b4f9217132ccdbfbe4e579f0d9ecda6c
vukha12 <kha15964@gmail.com>
2025-10-15 08:37:03 +0700

    done prev-next-post

--- wordpress_data/wp-content/themes/twentytwenty/template-parts/content.php
+++ wordpress_data/wp-content/themes/twentytwenty/template-parts/content.php
@@ -37,7 +37,7 @@ if (!is_single()) {
 		<?php endif; ?> -->
 
 		<!-- Nội dung bài viết -->
-		<div class="entry-content">
+		<div class="entry-content entry-content-home">
 			<h2 class="entry-title">
 				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 			</h2>
@@ -49,14 +49,50 @@ if (!is_single()) {
 
 	<?php else : ?>
 		<!-- Nếu đang ở trang chi tiết -->
-
-
 		<div class="entry-content">
 			<?php
 			get_template_part('template-parts/entry-header');
 			?>
 			<?php the_content(); ?>
 		</div>
+		<?php
+		$prev_post = get_previous_post();
+		$next_post = get_next_post();
+
+		if ($prev_post || $next_post): ?>
+			<div class="custom-post-navigation section-inner">
+
+				<?php if ($next_post): ?>
+					<div class="nav-item next-post">
+						<a href="<?php echo get_permalink($next_post->ID); ?>">
+							<div class="date">
+								<div class="day-month">
+									<span class="day"><?php echo get_the_date('d', $next_post->ID); ?></span>
+									<span class="month"><?php echo get_the_date('m', $next_post->ID); ?></span>
+								</div>
+								<span class="year"><?php echo get_the_date('y', $next_post->ID); ?></span>
+							</div>
+							<div class="title"><?php echo get_the_title($next_post->ID); ?></div>
+						</a>
+					</div>
+				<?php endif; ?>
+
+				<?php if ($prev_post): ?>
+					<div class="nav-item prev-post">
+						<a href="<?php echo get_permalink($prev_post->ID); ?>">
+							<div class="date">
+								<div class="day-month">
+									<span class="day"><?php echo get_the_date('d', $prev_post->ID); ?></span>
+									<span class="month"><?php echo get_the_date('m', $prev_post->ID); ?></span>
+								</div>
+								<span class="year"><?php echo get_the_date('y', $prev_post->ID); ?></span>
+							</div>
+							<div class="title"><?php echo get_the_title($prev_post->ID); ?></div>
+						</a>
+					</div>
+				<?php endif; ?>
+			</div>
+		<?php endif; ?>
 
 	<?php endif; ?>
 
--- wordpress_data/wp-content/themes/twentytwenty/template-parts/pagination.php
+++ wordpress_data/wp-content/themes/twentytwenty/template-parts/pagination.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * A template partial to output pagination for the Twenty Twenty default theme.
  *
@@ -16,7 +17,7 @@ $prev_text = sprintf(
 	 * Translators: This text contains HTML to allow the text to be shorter on small screens.
 	 * The text inside the span with the class nav-short will be hidden on small screens.
 	 */
-	__( 'Newer <span class="nav-short">Posts</span>', 'twentytwenty' )
+	__('Newer <span class="nav-short">Posts</span>', 'twentytwenty')
 );
 $next_text = sprintf(
 	'<span class="nav-next-text">%s</span> %s',
@@ -24,7 +25,7 @@ $next_text = sprintf(
 	 * Translators: This text contains HTML to allow the text to be shorter on small screens.
 	 * The text inside the span with the class nav-short will be hidden on small screens.
 	 */
-	__( 'Older <span class="nav-short">Posts</span>', 'twentytwenty' ),
+	__('Older <span class="nav-short">Posts</span>', 'twentytwenty'),
 	'<span aria-hidden="true">&rarr;</span>'
 );
 
@@ -37,24 +38,24 @@ $posts_pagination = get_the_posts_pagination(
 );
 
 // If we're not outputting the previous page link, prepend a placeholder with `visibility: hidden` to take its place.
-if ( false === strpos( $posts_pagination, 'prev page-numbers' ) ) {
-	$posts_pagination = str_replace( '<div class="nav-links">', '<div class="nav-links"><span class="prev page-numbers placeholder" aria-hidden="true">' . $prev_text . '</span>', $posts_pagination );
+if (false === strpos($posts_pagination, 'prev page-numbers')) {
+	$posts_pagination = str_replace('<div class="nav-links">', '<div class="nav-links"><span class="prev page-numbers placeholder" aria-hidden="true">' . $prev_text . '</span>', $posts_pagination);
 }
 
 // If we're not outputting the next page link, append a placeholder with `visibility: hidden` to take its place.
-if ( false === strpos( $posts_pagination, 'next page-numbers' ) ) {
-	$posts_pagination = str_replace( '</div>', '<span class="next page-numbers placeholder" aria-hidden="true">' . $next_text . '</span></div>', $posts_pagination );
+if (false === strpos($posts_pagination, 'next page-numbers')) {
+	$posts_pagination = str_replace('</div>', '<span class="next page-numbers placeholder" aria-hidden="true">' . $next_text . '</span></div>', $posts_pagination);
 }
 
-if ( $posts_pagination ) { ?>
+if ($posts_pagination) { ?>
 
 	<div class="pagination-wrapper section-inner">
 
 		<hr class="styled-separator pagination-separator is-style-wide" aria-hidden="true" />
 
-		<?php echo $posts_pagination; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- already escaped during generation. ?>
+		<?php echo $posts_pagination; ?>
 
 	</div><!-- .pagination-wrapper -->
 
-	<?php
+<?php
 }
